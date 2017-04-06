6bf6f18a67259aba5034d738b6b0e66b843e2739
Iaroslav Ciupin <Iaroslav.Ciupin@endava.com>
2017-04-06 21:57:28 +0300

    * Fetch rubbers * Use glide * Update .travis.yml

--- services/ttdb_rubber_fetching_service.go
+++ services/ttdb_rubber_fetching_service.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"github.com/YaroslavDev/table-tennis-db-crawler/model"
-	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"strconv"
+	"strings"
+	"log"
 )
 
 type TTDBRubberFetchingService struct {}
@@ -14,10 +16,37 @@ func (service TTDBRubberFetchingService) FetchRubbers() ([]model.Rubber, error) 
 		return nil, err
 	}
 
-	doc.Find("div #brand-andro").Each(func(i int, s *goquery.Selection) {
+	var rubbers []model.Rubber
+	var numRubbersFound int
+	doc.Find("div [id^=brand-]").Each(func(i int, s *goquery.Selection) {
 		s.Find("tr:not([class])").Each(func(i int, trSelection *goquery.Selection) {
-			fmt.Println(trSelection.Text() + "JORA")
+			rubber := model.Rubber{}
+			trSelection.Find("td").Each(func(i int, tdSelection *goquery.Selection) {
+				numRubbersFound++
+				tdText := tdSelection.Text()
+				tdText = strings.TrimSpace(tdText)
+				var parameter float32
+				if i > 0 {
+					parameter64, err := strconv.ParseFloat(tdText, 32)
+					if err != nil {
+						return
+					}
+					parameter = float32(parameter64)
+				}
+				switch i {
+				case 0:
+					rubber.Name = tdText
+				case 1:
+					rubber.Speed = parameter
+				case 2:
+					rubber.Spin = parameter
+				case 3:
+					rubber.Tackiness = parameter
+				}
+			})
+			rubbers = append(rubbers, rubber)
 		})
 	})
-	return nil, nil
+	log.Printf("Found in total %d rubbers!", numRubbersFound)
+	return rubbers, nil
 }
